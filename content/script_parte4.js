1acec41fbbe4e405447e6d069709475c189804d7
Hamander <1hamander@gmail.com>
2021-03-24 18:06:04 -0300

    Parte_4 Concluida

---
+++ content/script_parte4.js
@@ -0,0 +1,15 @@
+let processo_Seletivo = '';
+
+switch (processo_Seletivo) {
+    case 'aprovada':
+      console.log('Aprovada(o)');
+      break;
+    case 'lista':
+      console.log('Lista');
+      break;
+    case 'reprovada':
+      console.log('Reprovada(o)');
+      break;
+    default:
+      console.log('Não se Aplica')
+}
