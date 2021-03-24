2d92482fad5aa817bc9c6e77b333e89665fa7660
Hamander <1hamander@gmail.com>
2021-03-24 15:56:46 -0300

    Variavel Perimetro Adicionada

--- content/script_parte2.js
+++ content/script_parte2.js
@@ -1,7 +1,9 @@
 let base = 5;
 let altura = 8;
 let area = base * altura;
+let perimetro = (base * 2) + (altura *2);
 
 console.log(base);
 console.log(altura);
 console.log(area);
+console.log(perimetro);
