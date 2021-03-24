9c8edfc5626204f16ea7e1030ad43563547998d7
Hamander <1hamander@gmail.com>
2021-03-24 18:05:48 -0300

    Parte_4 Concluida

--- content/script_parte3.js
+++ content/script_parte3.js
@@ -1,9 +1,9 @@
-let candidato_nota = 70;
+let candidato_nota = 80;
 
 if (candidato_nota >= 80) {
     console.log("Parabéns, você foi aprovada(o)!")
 } 
-else if (candidato_nota <= 80 && candidato_nota > 60) {
+else if (candidato_nota <= 80 && candidato_nota >= 60) {
     console.log("Você está na nossa lista de espera.")
 } 
 else if (candidato_nota < 60) {
