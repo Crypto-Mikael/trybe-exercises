53bdc051414e6d96e918a73dc9047559e9dddda5
Hamander <1hamander@gmail.com>
2021-03-24 15:14:11 -0300

    Console.log variavel repetida solucionada

--- exercises/scripts.js
+++ exercises/scripts.js
@@ -1,5 +1,5 @@
 const my_name = "Miguel da Costa Campos";
 const birthCity = "Santô Antônio de Pádua";
-let birthYear = 2002
+let birthYear = 2020
 
-console.log(my_name ,birthCity, birthCity)
+console.log(my_name ,birthCity, birthYear)
