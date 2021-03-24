7882b268587bdedb1a3e01ba0aaa2e3b43c21a39
Hamander <1hamander@gmail.com>
2021-03-24 16:29:52 -0300

    Estrutura Else if Adicionada

---
+++ content/script_parte3.js
@@ -0,0 +1,11 @@
+let candidato_nota = 70;
+
+if (candidato_nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+} 
+else if (candidato_nota <= 80 && candidato_nota > 60) {
+    console.log("Você está na nossa lista de espera.")
+} 
+else if (candidato_nota < 60) {
+    console.log("Você foi reprovada(o)")
+} 
