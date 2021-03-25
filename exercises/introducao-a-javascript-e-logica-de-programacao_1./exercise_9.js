0d098c5b97aefe946d454e6316c5dfa536c515e5
Hamander <1hamander@gmail.com>
2021-03-24 21:33:45 -0300

    Exercicio 9 Completo. Teste do Impar

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1./exercise_9.js
@@ -0,0 +1,13 @@
+let num1 = 2;
+let num2 = 4;
+let num3 = 8;
+
+modulo1 = num1 % 2
+modulo2 = num2 % 2
+modulo3 = num3 % 2
+
+if (modulo1 != 0 || modulo2 != 0 || modulo3 != 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
