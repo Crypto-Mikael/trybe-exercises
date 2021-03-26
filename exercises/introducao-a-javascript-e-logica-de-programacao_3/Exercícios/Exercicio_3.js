7128f04a652827f861fd9fef7e4f032279049629
Hamander <1hamander@gmail.com>
2021-03-26 18:14:08 -0300

    Exercicio_3 Completo. Criando piramide ao lado esquerdo

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/Exercícios/Exercicio_3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/Exercícios/Exercicio_3.js
@@ -3,5 +3,5 @@ let space = ' ';
 let ast = '*';
 
 for (let width = 1; width <= height; width += 1) { 
-  console.log(ast.repeat(width)) 
+  console.log(space.repeat(height - width),ast.repeat(width)) 
 }
