15f798dec5da1f3d95f59222d4781d30648df741
Hamander <1hamander@gmail.com>
2021-03-29 19:16:45 -0300

    Exercicio_3 Parte_4.Function add.

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Exercicios/Exercicio_4_Parte_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Exercicios/Exercicio_4_Parte_2.js
@@ -1,12 +1,14 @@
 let names = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
-let letters = names[0].length
-for (let index = 0; index < names.length; index += 1) {
+function bigestName () {
+  let letters = names[0].length
+  for (let index = 0; index < names.length; index += 1) {
 
     if (names[index].length > letters) {
         letters = names[index].length
         name = names[index]
+        };
     };
-};
-
-console.log(name)
+    return name
+}
+console.log(bigestName())
