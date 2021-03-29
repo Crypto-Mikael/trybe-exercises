f78ce7e6c3587c43b36d453835da51733ed74a05
Hamander <1hamander@gmail.com>
2021-03-29 15:29:01 -0300

    Array in Object

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Pratica/Pratica_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Pratica/Pratica_1.js
@@ -6,6 +6,9 @@ let player = {
     medals: {
         golden: 2,
         silver: 3,
-    }
+    },
+    bestInTheWorld: ['2006','2007','2008','2009','2010','2018']
 };
+
 console.log(`A jogadora ${player.name} ${player.lastName}, ${player.age} tem ${player.age} anos de idade.`)
+console.log(`A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes.`)
