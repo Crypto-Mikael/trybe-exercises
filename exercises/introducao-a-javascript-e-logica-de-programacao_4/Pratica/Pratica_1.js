a725dfa54101478a1d9dd94b688c640ad91c7245
Hamander <1hamander@gmail.com>
2021-03-29 15:14:56 -0300

    Pratica de criaçao de Objetos.

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Pratica/Pratica_1.js
@@ -0,0 +1,11 @@
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    }
+};
+console.log(`A jogadora ${player.name} ${player.lastName}, ${player.age} tem ${player.age} anos de idade.`)
