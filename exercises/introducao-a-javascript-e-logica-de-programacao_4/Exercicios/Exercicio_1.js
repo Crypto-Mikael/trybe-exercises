ec82512a9abe903b56b4c68c771ba8ac93b6509c
Hamander <1hamander@gmail.com>
2021-03-29 16:24:07 -0300

    Exercicio_4 e 5  Parte 1 Completo.

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Exercicios/Exercicio_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Exercicios/Exercicio_1.js
@@ -1,4 +1,5 @@
-let info = {
+/*
+ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
@@ -10,3 +11,15 @@ let info = {
   for (const key in info) {
   console.log(info[key])
   }
+  
+*/
+let info = {
+  personagem: 'Margarida e Tio Patinhas',
+  origem: ['Pato Donald','Christmas on Bear Moutain',`Dell's Four Color Comics #178`],
+  nota: ['O último MacPatinhas','Namorada do personagem principal nos quadrinhos do Pato Donald'],
+  recorrente:'Ambos recorrentes',
+}
+
+for (const key in info) {
+  console.log(info[key])
+}
