c630abbbe0c5958cee071683aae93810e55c9948
Hamander <1hamander@gmail.com>
2021-03-29 16:14:26 -0300

    Exercicio_3 Parte 1 Completo.

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Exercicios/Exercicio_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Exercicios/Exercicio_1.js
@@ -4,5 +4,9 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim',
   };
-  console.log(`Bem vinda, ${info.personagem}!`)
-  console.log(info.recorrente)
+  console.log(`Bem vinda, ${info.personagem}!`);
+  console.log(info.recorrente);
+
+  for (const key in info) {
+  console.log(info[key])
+  }
