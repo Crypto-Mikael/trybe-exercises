cc37ef59edb9e4d6316c0c6b7a0170cdc32d729e
Hamander <1hamander@gmail.com>
2021-04-05 15:41:04 -0300

    Re-faturaçao dos Nomes.

--- exercises/javascript-dom-eventos-e-web-storage_1/Prática/scripts.js
+++
@@ -1,7 +0,0 @@
-let XMEN = document.getElementById('page-title').innerHTML = 'X-Men Evolution'
-let E = document.getElementById("second-paragraph");
-      paragraph.innerHTML = 'Abertura' 
-      paragraph.style.color = "purple";
-let MUITOBOM = document.getElementById('subtitle');
-      paragraph.innerHTML = 'POG'
-      paragraph.style.color = 'purple'
