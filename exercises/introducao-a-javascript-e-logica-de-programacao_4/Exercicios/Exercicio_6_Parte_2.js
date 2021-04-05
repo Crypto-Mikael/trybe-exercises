7963481b70523519caba62b896ebc7ef36fd9fa6
Hamander <1hamander@gmail.com>
2021-04-05 14:55:25 -0300

    Documentos Excluidos

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Exercicios/Exercicio_6_Parte_2.js
+++
@@ -1,9 +0,0 @@
-
-function numberN (n) {
-    let finalnumber = 0 
-    for (index = 0; index < n; index += 1) {
-        finalnumber = finalnumber + n;
-    }
-    return finalnumber;
-}
-console.log(numberN(6))
