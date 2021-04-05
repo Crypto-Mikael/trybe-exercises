6c2d66f8b5b7a6ed27435f374ddb1592186d45c3
Hamander <1hamander@gmail.com>
2021-04-05 15:27:58 -0300

    Conteudo Parte_2. Pratica 2.

---
+++ exercises/javascript-dom-eventos-e-web-storage_1/Prática/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('page-title').innerHTML = 'X-Men Evolution'
+document.getElementById("second-paragraph");
+      paragraph.innerHTML = 'Abertura' 
+      paragraph.style.color = "purple";
+document.getElementById('subtitle');
+      paragraph.innerHTML = 'POG'
+      paragraph.style.color = 'purple'
