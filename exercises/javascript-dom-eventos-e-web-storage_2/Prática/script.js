402930ab8852d121937f64e46316c6a74316f129
Hamander <1hamander@gmail.com>
2021-04-06 15:57:25 -0300

    Parte I - Praticando parentNode. Parte 6

--- exercises/javascript-dom-eventos-e-web-storage_2/Prática/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/Prática/script.js
@@ -7,13 +7,15 @@ ElementFather.style.color = 'orange'
 
 // 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
 let grandchildtxt = document.getElementById('primeiroFilhoDoFilho') 
-grandchildtxt.innerHTML = 'Eu sou um neto muito querido.'
+grandchildtxt.innerText = 'Eu sou um neto muito querido.'
 
 // 4. Acesse o primeiroFilho a partir de pai.
 document.getElementById('pai').childNodes[1]
 
-// 5.Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+// 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
 document.getElementById('elementoOndeVoceEsta').parentNode.childNodes[1]
 
+// 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+document.getElementById('elementoOndeVoceEsta').parentNode.innerText
 
 
