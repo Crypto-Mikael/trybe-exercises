1cc273d1584cea92279e03fd0ca91ffe8563deba
Hamander <1hamander@gmail.com>
2021-04-06 15:29:38 -0300

    Parte I - Praticando parentNode. Parte 2

--- exercises/javascript-dom-eventos-e-web-storage_2/Prática/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/Prática/script.js
@@ -1,1 +1,5 @@
+// 1. Acesse o elemento elementoOndeVoceEsta .
 document.querySelector('#elementoOndeVoceEsta')
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+let ElementFather = document.querySelector('#elementoOndeVoceEsta').parentNode
+ElementFather.style.color = 'orange'
