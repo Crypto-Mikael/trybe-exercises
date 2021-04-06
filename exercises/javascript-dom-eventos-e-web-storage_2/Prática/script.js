5f509902ea390ddb613c499f142e86dd21243547
Hamander <1hamander@gmail.com>
2021-04-06 16:31:59 -0300

    Parte II - Praticando parentNode. Parte 1

--- exercises/javascript-dom-eventos-e-web-storage_2/Prática/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/Prática/script.js
@@ -1,26 +1,38 @@
-// 1. Acesse o elemento elementoOndeVoceEsta .
-document.getElementById('elementoOndeVoceEsta')
+// Parte I
+
+// 1. Acesse o elemento elementoOndeVoceEsta.
+document.getElementById('elementoOndeVoceEsta');
 
 // 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-let ElementFather = document.getElementById('elementoOndeVoceEsta').parentNode
-ElementFather.style.color = 'orange'
+let ElementFather = document.getElementById('elementoOndeVoceEsta').parentNode;
+ElementFather.style.color = 'orange';
 
 // 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
-let grandchildtxt = document.getElementById('primeiroFilhoDoFilho') 
-grandchildtxt.innerText = 'Eu sou um neto muito querido.'
+let grandchildtxt = document.getElementById('primeiroFilhoDoFilho') ;
+grandchildtxt.innerText = 'Eu sou um neto muito querido.';
 
 // 4. Acesse o primeiroFilho a partir de pai.
-document.getElementById('pai').childNodes[1]
+document.getElementById('pai').childNodes[1];
 
 // 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
-document.getElementById('elementoOndeVoceEsta').parentNode.childNodes[1]
+document.getElementById('elementoOndeVoceEsta').parentNode.childNodes[1];
 
 // 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
-document.getElementById('elementoOndeVoceEsta').parentNode.innerText
+document.getElementById('elementoOndeVoceEsta').parentNode.innerText;
 
 // 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
-document.getElementById('elementoOndeVoceEsta').parentNode.childNodes[5]
+document.getElementById('elementoOndeVoceEsta').parentNode.childNodes[5];
 
 // 8.Agora acesse o terceiroFilho a partir de pai.
-document.getElementById('pai').childNodes[5]
+document.getElementById('pai').childNodes[5];
+
+// Parte II
+
+// 1. Crie um irmão para elementoOndeVoceEsta.
+let divCreator = document.createElement('div');
+let addDivOnElement = document.querySelector('#elementoOndeVoceEsta').appendChild(divcreator);
+
+
+
+
 
