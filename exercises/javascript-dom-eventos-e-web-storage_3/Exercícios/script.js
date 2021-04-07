eff03909d8761880157182461aebdf4cfb2cfef6
Hamander <1hamander@gmail.com>
2021-04-07 20:26:57 -0300

    Exercicio_1 Completo. Base do Exercicio 1 Adicionada.

--- exercises/javascript-dom-eventos-e-web-storage_3/Exercícios/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/Exercícios/script.js
@@ -14,12 +14,43 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
 
   // Desenvolva uma fucao que crie cada dia do caledário;
+
   function creatDayOfTheCalendar() {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-    let days = document.querySelector('#days');
-    let day = document.createElement('li');
 
-    for (let index = 0; index > dezDaysList.length; index += 1) {
-      day
-    }  
-  }
+    let month = document.querySelector('#days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+      let day = dezDaysList[index];
+      let daysListItem = document.createElement('li');
+      daysListItem.innerText = day;
+      daysListItem.classList = 'day'
+
+      if (daysListItem.innerText === '24') {
+        daysListItem.classList.add(['holiday']) 
+      }
+      else if (daysListItem.innerText === '25') {
+        daysListItem.classList.add(['holiday']) 
+      }
+      else if (daysListItem.innerText === '31') {
+        daysListItem.classList.add(['holiday']) 
+      }
+
+      if (daysListItem.innerText === '4') {
+        daysListItem.classList.add(['friday']) 
+      }
+      else if (daysListItem.innerText === '11') {
+        daysListItem.classList.add(['friday']) 
+      }
+      else if (daysListItem.innerText === '18') {
+        daysListItem.classList.add(['friday']) 
+      }
+      else if (daysListItem.innerText === '25') {
+        daysListItem.classList.add(['friday']) 
+      }
+      
+      month.appendChild(daysListItem);
+    };
+  };
+
+  creatDayOfTheCalendar()
