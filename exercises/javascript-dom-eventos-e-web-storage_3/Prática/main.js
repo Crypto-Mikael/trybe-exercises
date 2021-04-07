faad88b6f5ac979eadc263540586b13f5c770913
Hamander <1hamander@gmail.com>
2021-04-07 18:17:27 -0300

    Pratica Extendida

--- exercises/javascript-dom-eventos-e-web-storage_3/Prática/main.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/Prática/main.js
@@ -4,20 +4,28 @@ const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-/*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
 
-Segue abaixo um exemplo do uso de event.target:
-*/
+// 1. Copie esse arquivo e edite apenas ele;
+
+// 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+  let clickP = document.querySelector('.container')
+
+  clickP.addEventListener('click', function (event) {  
+    event.target.className = 'tech';
+  });
+ 
+
+//3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+//   com a classe 'tech';
+
+  let writeP =  document.querySelector('#input')
+
+  let writed = writeP.addEventListener('keyup', keyreceiver)
+  function keyreceiver (eventKey) {
+  }
+
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
