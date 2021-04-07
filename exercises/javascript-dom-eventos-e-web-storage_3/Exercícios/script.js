5eeb3ca8b89977425c005133aaa787e24a4e755e
Hamander <1hamander@gmail.com>
2021-04-07 18:49:42 -0300

    Exerciocio 1

--- exercises/javascript-dom-eventos-e-web-storage_3/Exercícios/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/Exercícios/script.js
@@ -12,3 +12,14 @@ function createDaysOfTheWeek() {
   };
   
   createDaysOfTheWeek();
+
+  // Desenvolva uma fucao que crie cada dia do caledário;
+  function creatDayOfTheCalendar() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    let days = document.querySelector('#days');
+    let day = document.createElement('li');
+
+    for (let index = 0; index > dezDaysList.length; index += 1) {
+      day
+    }  
+  }
