7f1ab1c5264d510e60ef4c8dd472f021cc48d530
Hamander <1hamander@gmail.com>
2021-04-16 15:49:01 -0300

    Bulma Tests

---
+++ exercises/html-e-css-forms-flexbox-e-reponsivo.2/Prática/script.js
@@ -0,0 +1,40 @@
+function creatOptions() {
+    const locateCreatTagSelected = document.querySelector('#states');
+    const states = ['Acre',
+      'Alagoas',
+      'Amapá',
+      'Amazonas',
+      'Bahia',
+      'Ceará',
+      'Distrito Federal',
+      'Espirito Santo',
+      'Goiás',
+      'Maranhão',
+      'Mato Grosso do Sul',
+      'Mato Grosso',
+      'Minas Gerais',
+      'Pará',
+      'Paraíba',
+      'Paraná',
+      'Pernambuco',
+      'Piauí',
+      'Rio de Janeiro',
+      'Rio Grande do Norte',
+      'Rio Grande do Sul',
+      'Rondônia',
+      'Roraima',
+      'Santa Catarina',
+      'São Paulo',
+      'Sergipe',
+      'Tocantis']
+    for (let index = 0; index < states.length; index += 1) {
+      const CreatTagOptionOnSelect = document.createElement('option');
+      locateCreatTagSelected.appendChild(CreatTagOptionOnSelect);
+      CreatTagOptionOnSelect.innerHTML = states[index];
+    }
+  }
+  creatOptions();
+
+function preventDefautOptions() {
+    
+}
