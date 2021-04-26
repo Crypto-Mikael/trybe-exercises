f8f075c7c80d850aa2932e930632c2897b9c2acc
Hamander <1hamander@gmail.com>
2021-04-26 16:04:00 -0300

    Lint Fixes

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/Exercícios/script_2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/Exercícios/script_2.js
@@ -2,4 +2,4 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 oddsAndEvens.sort((primeiroElemento,segundoElemento) => primeiroElemento - segundoElemento);
 
-console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`)
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
