e2fd120ac4189e120df6eea5fd3a5c183f826e0d
Hamander <1hamander@gmail.com>
2021-04-26 16:02:18 -0300

    Exercicio_2 Sort Aplicado

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/Exercícios/script_2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/Exercícios/script_2.js
@@ -2,5 +2,4 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 oddsAndEvens.sort((primeiroElemento,segundoElemento) => primeiroElemento - segundoElemento);
 
-
-console.log(oddsAndEvens);
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`)
