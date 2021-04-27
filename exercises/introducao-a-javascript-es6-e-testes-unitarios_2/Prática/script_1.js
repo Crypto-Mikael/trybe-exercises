e10448a915a66909e2235290090ad44ba34f2008
Hamander <1hamander@gmail.com>
2021-04-27 13:03:07 -0300

    Leitura do Conteudo Iniciada

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/Prática/script_1.js
@@ -0,0 +1,9 @@
+function objectES6(objName,keyObjName,objValue) {
+    
+  objName = {
+    
+  }
+  objName[keyObjName] = objValue
+  return objName
+}
+console.log(objectES6('customer1','firstname','Roberto'))
