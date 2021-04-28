9195c84fd9cadbd676a97cb030a827d6d6c98c43
Hamander <1hamander@gmail.com>
2021-04-28 15:10:25 -0300

    Leitura do Conteudo 3

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/Prática/arrow_function_assert.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+const add = (a, b) => {a + b};
+
+const expected = add(1, 2);
+
+assert.ok(expected === 3, 'Um mais dois é igual a três'); // Checa se o primeiro argumento é verdadeiro
+assert.strictEqual(expected, 3, 'Um mais dois é igual a três'); // Checa se o primeiro e segundo argumentos são iguais em valor e tipo (===)
+assert.notStrictEqual(expected, 4, 'Um mais dois é igual a três (e não quatro!)'); // Checa se o primeiro e segundo argumentos são diferentes (!==)
