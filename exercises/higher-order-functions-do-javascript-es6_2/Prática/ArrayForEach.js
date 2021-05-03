2549ad9d7344e5a4ffb96fe11a144a55cc17f17d
Hamander <1hamander@gmail.com>
2021-05-03 15:12:53 -0300

    Leitura do Conteudo Iniciada

---
+++ exercises/higher-order-functions-do-javascript-es6_2/Prática/ArrayForEach.js
@@ -0,0 +1,18 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Use o método forEach chamando a callback showEmailList para apresentar os emails.
+
+emailListInData.forEach((item, position, array) => {
+  showEmailList(item);
+  console.log(`Position is equal: ${position}`)
+  console.log(`Number o email is equal: ${array.length - position}`)
+})
