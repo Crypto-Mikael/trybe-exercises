56bd7b9d9852e8e144989a81917fbd0cb9e3287b
Hamander <1hamander@gmail.com>
2021-05-05 17:52:12 -0300

    Atividade_5 Refatorada

--- exercises/higher-order-functions-do-javascript-es6.4/Exercícios/exercise_5.js
+++ exercises/higher-order-functions-do-javascript-es6.4/Exercícios/exercise_5.js
@@ -7,6 +7,6 @@ const names = [
 ];
 function containsA() {
   const stringLettersAppart = names.join('').toLowerCase().split('')
-  return stringLettersAppart.reduce((acumulator,resultadoAtual) => resultadoAtual === 'a' ? acumulator += 1 : acumulator += 0 ,0)
+  return stringLettersAppart.reduce((acumulator,actualResult) => actualResult === 'a' ? acumulator += 1 : acumulator += 0 ,0)
 }
 assert.deepStrictEqual(containsA(), 20);
