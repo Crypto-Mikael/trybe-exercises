8e3f9438e4393671aa466182a44dc8a45b53f8d7
Hamander <1hamander@gmail.com>
2021-05-06 20:08:50 -0300

    Pratica atualizada

--- exercises/higher-order-functions-do-javascript-es6_5/Prática/arrayDestructuring_3.js
+++ exercises/higher-order-functions-do-javascript-es6_5/Prática/arrayDestructuring_3.js
@@ -1,7 +1,7 @@
 let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
-let [one,three,five,six,eight,ten,twelve] = numerosPares
-numerosPares = [one,eight,ten,twelve]
+[,,,...numerosPares] = numerosPares
+
 
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
 
