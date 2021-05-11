dfa4194e8cf3b27853a0964da45b0d20c013515e
Hamander <1hamander@gmail.com>
2021-05-11 16:18:10 -0300

    Pratica callback 2

--- execises/javascript-assincorono-e-promises_1/Prática/Callbacks_2.js
+++ execises/javascript-assincorono-e-promises_1/Prática/Callbacks_2.js
@@ -3,13 +3,16 @@ const assert = require('assert');
 const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const getUser = () => {
+const getUser = (callback) => {
   const userToReturn = {
     firstName: "Ivan",
     lastName: "Ivanovich",
-    nationality: "Russian"
+    nationality: "Russian",
   };
+  return callback(userToReturn);
 };
+getUser(userFullName);
+getUser(userNationality);
 
-assert.strictEqual(getUser(), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
-assert.strictEqual(getUser(), "Ivan is Russian"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
