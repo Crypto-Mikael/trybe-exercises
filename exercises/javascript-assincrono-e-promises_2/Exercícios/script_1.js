7352c98987cb0f863185e5d21206bc60723f88c5
Hamander <1hamander@gmail.com>
2021-05-12 18:30:07 -0300

    Exercicios_1

---
+++ exercises/javascript-assincrono-e-promises_2/Exercícios/script_1.js
@@ -0,0 +1,8 @@
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+console.log(API_URL)
+const fetchJoke = () => {
+  // Adicionar lógica aqui!
+};
+
+window.onload = () => fetchJoke();
