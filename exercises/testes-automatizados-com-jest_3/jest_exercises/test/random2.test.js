b7663ec46f7bd0a2b5264ce3be375101e9047ae8
Hamander <1hamander@gmail.com>
2021-05-20 20:53:10 -0300

    Exercicio_1 Reafactora

--- exercises/testes-automatizados-com-jest_3/jest_exercises/test/random2.test.js
+++ exercises/testes-automatizados-com-jest_3/jest_exercises/test/random2.test.js
@@ -3,20 +3,20 @@ const { randomNumber } = require('../code/random');
 jest.mock('../code/random');
 
 describe('A função randomNumber retornar um numero aleatorio?', () => {
-  it('The function randomNumber returns number 53?', () => {
+  it('A função randomNumber retorna o numero 53?', () => {
     randomNumber.mockReturnValue(53);
     expect(randomNumber()).toBe(53);
   });
-  it('The function randomNumber returns number 64?', () => {
+  it('A função randomNumber retorna o numero 64?', () => {
     randomNumber.mockReturnValue(64);
     expect(randomNumber()).toBe(64);
   });
-  it('The function randomNumber returns number 82?', () => {
+  it('A função randomNumber retorna o numero 82?', () => {
     randomNumber.mockReturnValue(82);
     expect(randomNumber()).toBe(82);
   });
-  it('The function randomNumber returns number 31?', () => {
+  it('A função randomNumber retorna o numero 31?', () => {
     randomNumber.mockReturnValue(31);
     expect(randomNumber()).toBe(31);
   });
-});
+}); 
