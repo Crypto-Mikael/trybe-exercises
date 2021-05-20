c1e1b7e5a62fee6220b0afe6fc8d2701cc841ed2
Hamander <1hamander@gmail.com>
2021-05-20 15:00:23 -0300

    Exercicio_4 Completo

--- exercises/testes-automatizados-com-jest_2/jest_exercises/test/exercise.three.test.js
+++ exercises/testes-automatizados-com-jest_2/jest_exercises/test/exercise.three.test.js
@@ -1,12 +1,17 @@
 const { getRepos } = require('../code/exercise.three');
 
 describe('Returns the projects todo-list and project-meme', () => {
-  it('Get the repository names', () => {
-    const url = 'https://api.github.com/orgs/tryber/repos';
-    return getRepos(url).then(result => {
-      expect(result).toContain('sd-01-week4-5-project-todo-list');
-      expect(result).toContain('sd-01-week4-5-project-meme-generator');
-    });
-  });
+  const url = 'https://api.github.com/orgs/tryber/repos';
+  it('Return the project todo-list?', async () => {
+    const reposeOfUrl = await getRepos(url);
+    expect(reposeOfUrl).toContain('sd-01-week4-5-project-todo-list');
+  })
+  it('Return the project meme-generator?', async () => {
+    const reposeOfUrl = await getRepos(url);
+    expect(reposeOfUrl).toContain('sd-01-week4-5-project-meme-generator');
+  })
+  it('Return the projects length: 30', async () => {
+    const reposeOfUrl = await getRepos(url);
+    expect(reposeOfUrl.length).toBe(30);
+  })
 });
-  
