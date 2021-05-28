77a11c3827ebbc5c8356bcc25c241c9d5d9e515e
Hamander <1hamander@gmail.com>
2021-05-28 18:26:51 -0300

    Pokkemon first question resolves

--- exercises/Modulo_Front-end/Bloco_12/Estado-Eventos_Content_1/src/App.js
+++ exercises/Modulo_Front-end/Bloco_12/Estado-Eventos_Content_1/src/App.js
@@ -5,18 +5,27 @@ import './App.css';
 
 class App extends Component {
   constructor() {
-    super();
-    this.handleClick = this.handleClick.bind(this)
+    super();  
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      numerosDeCliques: 0,
+      color: 'black'
+    }
   }
 
   handleClick() {
-    console.log('oi')
+    this.setState((estate, _props) => ({
+      numerosDeCliques: estate.numerosDeCliques + 1,
+      color: estate.numerosDeCliques % 2 ? 'green' : 'black',
+    }))
+    console.log(this.state.color)
   }
 
   render() {
+    console.log(this)
     return (
       <div>
-        <button onClick={this.handleClick}>clicke aki!</button>
+        <button style={{color: this.state.color}} onClick={this.handleClick}>{this.state.numerosDeCliques}</button>
       </div>
       );
   }
