3d86ebac954f84392568dc2513d688164c4959c6
Hamander <1hamander@gmail.com>
2021-08-24 13:17:50 -0300

    Exercicio de Fixacao de Filtrando dados de Forma Agrupada

--- exercises/Modulo_Back-End/Dia_0/exerciciosDeFixação6.sql
+++ exercises/Modulo_Back-End/Dia_0/exerciciosDeFixação6.sql
@@ -23,3 +23,22 @@ SELECT district, COUNT(*)
 FROM sakila.address
 GROUP BY district
 ORDER BY COUNT(*) DESC;
+
+
+-- Exercicios de Fixacao
+
+-- 1. Usando a query a seguir, exiba apenas as durações médias que estão entre 115.0 a 121.50. Além disso, dê um alias (apelido) à coluna gerada por AVG(length) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma decrescente.
+
+SELECT rating, AVG(length) duracao_media
+FROM sakila.film
+GROUP BY rating
+HAVING duracao_media BETWEEN 115.0 AND 121.50
+ORDER BY duracao_media DESC;
+
+-- 2. Usando a query a seguir, exiba apenas os valores de total de substituição de custo que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
+
+SELECT rating, SUM(replacement_cost) as custo_de_substituicao
+FROM sakila.film
+GROUP BY rating
+HAVING custo_de_substituicao  > 3950.50
+ORDER BY custo_de_substituicao;
