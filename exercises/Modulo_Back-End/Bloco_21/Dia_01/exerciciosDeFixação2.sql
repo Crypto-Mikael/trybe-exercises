3caf8212f5ce1c75fd2e8433fd07b3efd7cfdf14
Hamander <1hamander@gmail.com>
2021-08-25 12:58:50 -0300

    Exercicios de Fixaçao de UNION E UNION ALL Completos

--- exercises/Modulo_Back-End/Bloco_21/Dia_01/exerciciosDeFixação2.sql
+++ exercises/Modulo_Back-End/Bloco_21/Dia_01/exerciciosDeFixação2.sql
@@ -8,5 +8,6 @@
 
 	SELECT t1.title, t1.length, t2.title, t2.length
 	FROM sakila.film AS t1, sakila.film AS t2
-    WHERE t1.rental_duration BETWEEN 2 AND 4 
-    AND t2.rental_duration BETWEEN 2 AND 4
+  WHERE t1.rental_duration BETWEEN 2 AND 4 
+  AND t2.rental_duration BETWEEN 2 AND 4;
+  
