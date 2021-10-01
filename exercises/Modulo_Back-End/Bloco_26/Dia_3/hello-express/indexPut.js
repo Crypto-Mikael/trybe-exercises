e290b46ad5311c2fc423ab79fb79b6b7f120ef51
Hamander <1hamander@gmail.com>
2021-10-01 15:53:15 -0300

    Leitura do Conteudo Finalizada

---
+++ exercises/Modulo_Back-End/Bloco_26/Dia_3/hello-express/indexPut.js
@@ -0,0 +1,11 @@
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
