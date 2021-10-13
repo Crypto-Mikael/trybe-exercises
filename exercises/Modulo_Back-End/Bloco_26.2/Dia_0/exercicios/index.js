c51b4f30fc05c509cef143ac82a5400b21f4d41a
Hamander <1hamander@gmail.com>
2021-10-13 13:37:42 -0300

    finalizando o dia

--- exercises/Modulo_Back-End/Bloco_26.2/Dia_0/exercicios/index.js
+++ exercises/Modulo_Back-End/Bloco_26.2/Dia_0/exercicios/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const userRoute = require('./routes/user');
 
 const app = express()
 const PORT = 3000;
 
+
 app.use(express.json());
 
-app.use('/user');
+app.use('/user', userRoute);
 
 app.listen(PORT, () => console.log(`Servidor Rodando na porta: ${PORT}`));
--- exercises/Modulo_Back-End/Bloco_26.2/Dia_0/exercicios/routes/user.js
+++ exercises/Modulo_Back-End/Bloco_26.2/Dia_0/exercicios/routes/user.js
@@ -1,3 +1,29 @@
-const route = require('express').Router();
+const router = require('express').Router();
+const MONGODB = '../models/middlewares/userMongo';
+const SQL = '../models/middlewares/userSQL';
+const { insertUser, getAllUsers, userPasswordChecker, userIDChecker } = require('../models/middlewares/userMongo');
 
-route.post('/user')
+
+router.post('/', userPasswordChecker, async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const resolve = await insertUser(firstName, lastName, email, password)
+    return res.status(200).json(resolve);
+  } catch (err) {
+    return res.status(500).json({ message: `Problema ao inserir item: ${err}`});
+  }
+});
+
+router.get('/', async (_req, res) => {
+  const users = await getAllUsers();
+  return res.status(200).json(users)
+});
+
+router.get('/:id', userIDChecker, async (req, res) => {
+  const { id } = req.params;
+  const users = await getAllUsers();
+  const result = users.find((user) => String(user._id) === id) 
+  res.status(200).json(result);
+})
+
+module.exports = router;
