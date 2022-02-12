e3e3f5dfaa150ed69f359d253721e33a9faca077
Hamander <1hamander@gmail.com>
2022-02-12 19:29:59 -0300

    aulas assitidas

---
+++ exercises/Ciencia-da-Computacao/bloco-35-arquitetura-de-redes/dia-1-arquitetura-de-redes/Aula 3/34-3/quotes/spider_quotes.py
@@ -0,0 +1,38 @@
+import requests
+import parsel
+
+def fetch_content(url, timeout=1):
+	try:
+		response = requests.get(url, timeout=timeout)
+		response.raise_for_status()
+	except (requests.HTTPError, requests.ReadTimeout):
+		return ""
+	else:
+		return response.text
+
+def extract_quotes(text):
+	selector = parsel.Selector(text)
+	quotes = []
+
+	for quote in selector.css("div.quote"):
+		text = quote.css("span.text::text").get()
+		author = quotes.css("small.author::text").get()
+		tags = quotes.css("a.tag::text").getall()
+		quotes.append(
+			{ 
+				"text": text,
+				"author": author,
+				"tags": tags,
+			}
+		)
+	return quotes
+
+def get_all_quotes():
+	base_url = "https://quotes.toscrape.com"
+	next_page = "/"
+	quotes = []
+	while next_page:
+		content = fetch_content(base_url + next_page)
+		quotes.extend(extract_quotes(content))
+		next_page = parsel.Selector(content).css("li.next > a::attr(href)").get()
+	return quotes
